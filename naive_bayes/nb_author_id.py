3ac1ba50e430577011bdee92e0c2133833ca42f5
Devoshree Mukherji <Devoshree@users.noreply.github.com>
2017-03-19 14:56:31 +0530

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
@@ -11,10 +9,12 @@
 """
     
 import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+from time import time
+sys.path.append("....\tools\...")
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -26,8 +26,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+def nb_text_test():
+	
+	
+	clf = GaussianNB()
+	clf.fit(features_train,labels_train)
+	pred = clf.predict(features_test)
+	accuracy = accuracy_score(pred,labels_test)
+	return accuracy
 #########################################################
 
-
+sc = nb_text_test()
+print sc
+#########################################################
